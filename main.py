629581839b357051f20b7806839b75b970bdda65
Swapnil Bhosale <vb15736@umbc.edu>
2020-04-23 23:41:11 -0400

    minor change

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import matplotlib.image as mpimg
 import numpy as np
-import skimage
-import defaultdict
+from skimage import io
+from skimage.transform import resize
+from collections import defaultdict 
 
-def compute_pdfs(imfile, imfile_scrib):
+
+'''def compute_pdfs(imfile, imfile_scrib):
      rgb = mpimg.imread(imfile)[:,:,:3]
      yuv = skimage.color.rgb2yiq(rgb)
      rgb_s = mpimg.imread(imfile_scrib)[:,:,:3]
@@ -23,7 +25,11 @@ def compute_pdfs(imfile, imfile_scrib):
           mu[c] = np.mean(comps[c], axis=0)
           Sigma[c] = np.cov(comps[c].T)
      return (mu, Sigma)
+'''
+img = io.imread('test1.jpg')
+print(img.dtype, img.shape)
+print(img[0][0:2])
+#small_resized = resize(img, (30,30), mode='symmetric', preserve_range=True)
 
-rgb = mpimg.imread("test.png")[:,:,:3]
-yuv = skimage.color.rgb2yiq(rgb)
-print(rgb[0][:])
+# Write the tinted image back to disk
+#io.imsave('test1_scaled.jpg', small_resized)
